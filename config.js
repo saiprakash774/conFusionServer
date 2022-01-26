a30253cb81d147f680547175b640bb7e01a5ff05
Sai Prakash Sasubilli <sai.sasubilli@pearson.com>
2022-01-26 12:31:43 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3240097842919840',
+        clientSecret: '23c2ec85c06ebae9e3f0c06ea56dbb90'
+    }
 }
